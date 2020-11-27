9e3bd15b4a660807923f230b0fd368dedfd79e19
Liam <liam.s.oconnell@gmail.com>
2020-11-27 11:06:54 +1100

    Hexagon centering on grid and locking

--- script/Hex.gd
+++ script/Hex.gd
@@ -5,3 +5,15 @@ var grid_coordinate : Vector2
 func _ready():
 	$Label.text = (grid_coordinate.x as String) + "," + (grid_coordinate.y as String)
 	return
+
+
+func _on_Area2D_body_shape_entered(body_id, body, body_shape, area_shape):
+	body.call("add_hex_to_list", self)
+	return
+	
+	
+
+
+func _on_Area2D_body_shape_exited(body_id, body, body_shape, area_shape):
+	body.call("remove_hex_from_list", self)
+	return
--- script/Tile.gd
+++ script/Tile.gd
@@ -1,6 +1,9 @@
 extends KinematicBody2D
 
 var is_dragging = false
+var locked = false
+
+var colliding_hexes = []
 
 func _ready():
 	pass
@@ -17,6 +20,31 @@ func _physics_process(delta):
 
 func _on_Tile_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
-		if event.button_index == BUTTON_LEFT:
+		if event.button_index == BUTTON_LEFT and !locked:
 			is_dragging = event.pressed
+			if not event.pressed:
+				center_on_hex()
+	return
+
+func center_on_hex():
+	if !colliding_hexes.empty():
+		var closest_hex = Vector2(100000,100000)
+		for hex in colliding_hexes:
+			if hex.position.distance_to(position) < closest_hex.distance_to(position):
+				closest_hex = hex.position
+		position = closest_hex
+		locked = true
+	return
+
+func add_hex_to_list(hex : Node):
+	colliding_hexes.append(hex)
+	print(colliding_hexes)
+	return
+	
+func remove_hex_from_list(hex : Node):
+	var search_result = colliding_hexes.find(hex)
+	if search_result != -1:
+		colliding_hexes.remove(search_result)
+		
+	print(colliding_hexes)
 	return
