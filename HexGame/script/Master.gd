1ee51eac4724d261ec4e4df25b3bc4ea6d91a6dc
Liam <liam.s.oconnell@gmail.com>
2021-05-19 14:29:46 +1000

    Set up audio buses and options screen

--- HexGame/script/Master.gd
+++ HexGame/script/Master.gd
@@ -8,6 +8,7 @@ var playtest_cache
 
 func _ready():
 	Global.sound_mgr = $SoundManager
+	load_option_settings()
 	import_level_list()
 	load_main_menu()
 	return
@@ -88,4 +89,26 @@ func _input(event):
 			spark.position = event.position
 			spark.emitting = true
 			add_child(spark)
+	return
+
+func load_option_settings():
+	var saved_options = File.new()
+	if not saved_options.file_exists("user://options.dat"):
+		return
+	saved_options.open("user://options.dat", File.READ)
+	while saved_options.get_position() < saved_options.get_len():
+		
+		var saved_sfx_volume = parse_json(saved_options.get_line())
+		var SFX_bus = AudioServer.get_bus_index("SFX")
+		AudioServer.set_bus_volume_db(SFX_bus,saved_sfx_volume)
+		
+		var saved_music_volume = parse_json(saved_options.get_line())
+		var Music_bus = AudioServer.get_bus_index("Music")
+		AudioServer.set_bus_volume_db(Music_bus,saved_music_volume)
+		
+		OS.window_fullscreen = parse_json(saved_options.get_line())
+		var width = parse_json(saved_options.get_line())
+		var height = parse_json(saved_options.get_line())
+		OS.window_size = Vector2(width, height)
+	saved_options.close()
 	return
--- HexGame/script/Options.gd
+++ HexGame/script/Options.gd
@@ -1,13 +1,152 @@
 extends Node
 
-signal escape_pressed
+signal options_concluded
+
+
+var SFX_bus = AudioServer.get_bus_index("SFX")
+var Music_bus = AudioServer.get_bus_index("Music")
+
+var original_sfx_db : float
+var original_music_db : float
+var original_fullscreen : bool
+var original_resolution : Vector2
+
+#So we can filter resolution to numbers only
+onready var regex = RegEx.new()
 
 func _ready():
-	connect("escape_pressed", get_parent(), "load_main_menu")
+	load_options()
+	connect("options_concluded", get_parent(), "load_main_menu")
+	regex.compile("[0-9]+")
 	return
 	
 func _input(event):
 	if event is InputEventKey:
 		if event.scancode == KEY_ESCAPE and event.pressed:
-			emit_signal("escape_pressed")
+			emit_signal("options_concluded")
+	return
+
+
+func _on_Fullscreen_toggled(button_pressed):
+	OS.window_fullscreen = button_pressed
+	set_initial_resolution()
+	return
+
+
+func _on_SFXVolume_value_changed(value):
+	var max_value = get_node("VBoxContainer/SFXContainer/SFXVolume").max_value
+	var db_value = (value / (max_value / 60)) - 60
+	AudioServer.set_bus_volume_db(SFX_bus,db_value)
+	Global.sound_mgr.playMainMenuClick()
+	return
+
+
+func _on_MusicVolume_value_changed(value):
+	var max_value = get_node("VBoxContainer/MusicContainer/MusicVolume").max_value
+	var db_value = (value / (max_value / 60)) - 60
+	AudioServer.set_bus_volume_db(Music_bus,db_value)
+	return
+
+
+func _on_Width_text_changed(new_text):
+	var line_edit = get_node("VBoxContainer/ResolutionContainer/Width")
+	line_edit.text = remove_non_numbers(new_text)
+	line_edit.caret_position = line_edit.text.length()
+	return
+
+
+func _on_Height_text_changed(new_text):
+	var line_edit = get_node("VBoxContainer/ResolutionContainer/Height")
+	line_edit.text = remove_non_numbers(new_text)
+	line_edit.caret_position = line_edit.text.length()
+
+	return
+
+func remove_non_numbers(field_text):
+	var matches = regex.search_all(field_text)
+	var text = ""
+	if matches:
+		for r_match in matches:
+			text += r_match.get_string()
+			
+	return text
+
+func _on_Accept_pressed():
+	Global.sound_mgr.playMainMenuClick()
+	change_resolution()
+	save_options()
+	emit_signal("options_concluded")
+	return
+
+
+func _on_Cancel_pressed():
+	Global.sound_mgr.playMainMenuClick()
+	emit_signal("options_concluded")
+	return
+
+func change_resolution():
+	var width = get_node("VBoxContainer/ResolutionContainer/Width").text
+	var height = get_node("VBoxContainer/ResolutionContainer/Height").text
+	var resolution = Vector2(width, height)
+	if(resolution.x > OS.get_screen_size().x or resolution.y > OS.get_screen_size().y):
+		printerr("Resolution set is larger than screen size")
+	else:
+		OS.window_size = resolution
+	return
+	
+func set_initial_resolution():
+	#Set resolution initial value
+	original_resolution = OS.window_size
+	var width_text_box = get_node("VBoxContainer/ResolutionContainer/Width")
+	var height_text_box = get_node("VBoxContainer/ResolutionContainer/Height")
+	width_text_box.text = original_resolution.x as String
+	height_text_box.text = original_resolution.y as String
+	return
+
+func save_options():
+	var options = File.new()
+	options.open("user://options.dat", File.WRITE)
+	options.store_line(to_json(AudioServer.get_bus_volume_db(SFX_bus)))
+	options.store_line(to_json(AudioServer.get_bus_volume_db(Music_bus)))
+	options.store_line(to_json(OS.window_fullscreen))
+	options.store_line(to_json(OS.window_size.x))
+	options.store_line(to_json(OS.window_size.y))
+	options.close()
+	return
+
+func load_options():
+	#Set sfx slider initial value
+	var sfx_slider = get_node("VBoxContainer/SFXContainer/SFXVolume")
+	var sfx_db_value = AudioServer.get_bus_volume_db(SFX_bus)
+	original_sfx_db = sfx_db_value
+	var sfx_slider_value = (sfx_db_value + 60) * (sfx_slider.max_value / 60)
+	sfx_slider.value = sfx_slider_value
+	
+	#Set music slider initial value
+	var music_slider = get_node("VBoxContainer/MusicContainer/MusicVolume")
+	var mus_db_value = AudioServer.get_bus_volume_db(Music_bus)
+	original_music_db = mus_db_value
+	var mus_slider_value = (mus_db_value + 60) * (music_slider.max_value / 60)
+	music_slider.value = mus_slider_value
+	
+	#Set fullscreen initial value
+	original_fullscreen = OS.window_fullscreen
+	var fullscreen_checkbox = get_node("VBoxContainer/Fullscreen")
+	fullscreen_checkbox.pressed = OS.window_fullscreen
+	
+	set_initial_resolution()
+	return
+
+
+func _on_Width_text_entered(_new_text):
+	change_resolution()
+	return
+
+
+func _on_Height_text_entered(new_text):
+	change_resolution()
+	return
+
+func _on_ResolutionTimer_timeout():
+	set_initial_resolution()
 	return
