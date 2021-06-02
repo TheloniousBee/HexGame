9be4ac2e3c46b1a16eb00ee6cb3de17738effe69
Liam <liam.s.oconnell@gmail.com>
2021-06-02 17:19:49 +1000

    Lots of UI changes.

--- HexGame/script/TimeControls.gd
+++ HexGame/script/TimeControls.gd
@@ -7,6 +7,13 @@ signal play_pressed
 signal pause_pressed
 signal forward_step_pressed
 
+const play_texture_normal = preload("res://UI/play.png")
+const play_texture_hover = preload("res://UI/play_hover.png")
+const play_texture_pressed = preload("res://UI/play_pressed.png")
+const pause_texture_normal = preload("res://UI/pause.png")
+const pause_texture_hover = preload("res://UI/pause_hover.png")
+const pause_texture_pressed = preload("res://UI/pause_pressed.png")
+
 func _ready():
 	connect("reverse_step_pressed", get_parent(), "reverse_turn")
 	connect("play_pressed", get_parent(), "start_play_timer")
@@ -18,7 +25,9 @@ func _on_Reverse_Step_pressed():
 	Global.sound_mgr.playUndoButton()
 	emit_signal("reverse_step_pressed")
 	if playing:
-		get_node("Play").text = "Play"
+		get_node("Play").texture_normal = pause_texture_normal
+		get_node("Play").texture_hover = pause_texture_hover
+		get_node("Play").texture_pressed = pause_texture_pressed
 		playing = false
 	return
 
@@ -26,11 +35,15 @@ func _on_Play_pressed():
 	Global.sound_mgr.playPauseButton()
 	if playing:
 		emit_signal("pause_pressed")
-		get_node("Play").text = "Play"
+		get_node("Play").texture_normal = play_texture_normal
+		get_node("Play").texture_hover = play_texture_hover
+		get_node("Play").texture_pressed = play_texture_pressed
 		playing = false
 	else:
 		emit_signal("play_pressed")
-		get_node("Play").text = "Pause"
+		get_node("Play").texture_normal = pause_texture_normal
+		get_node("Play").texture_hover = pause_texture_hover
+		get_node("Play").texture_pressed = pause_texture_pressed
 		playing = true
 	return
 
